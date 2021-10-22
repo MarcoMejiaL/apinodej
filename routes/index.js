24a1585330df8e28d8c6d2a23c8e1bdda4156192
Marco Antonio Mejia Lopez <tomatin77@gmail.com>
2021-10-22 18:51:34 -0500

    curso terminado

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,15 @@
 const express = require('express');
 
-const productsRouter = require('./productsRouter')
-const usersRouter = require('./usersRouter');
-const peopleRouter = require('./peopleRouter')
-const categoriesRouter = require('./categoriesRouter')
+const productsRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
+const usersRouter = require('./users.router');
 
-
-
-function routerApi(app){
+function routerApi(app) {
   const router = express.Router();
-app.use('/api/v1', router)
-
-router.use('/products',productsRouter);
-router.use('/users', usersRouter);
-router.use('/people', peopleRouter);
-router.use('/categories', categoriesRouter);
+  app.use('/api/v1', router);
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/users', usersRouter);
 }
-module.exports=routerApi;
+
+module.exports = routerApi;
